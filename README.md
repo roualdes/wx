01cb1c9d5e00c080cfab0c4cb29d381434d97686
Edward A. Roualdes <edward.roualdes@uky.edu>
2015-04-22 21:49:54 -0400

    wx seems to be working

---
+++ README.md
@@ -0,0 +1,31 @@
+# wx
+`wx`(.go) is a command-line application that retrieves [NOAA](http://www.weather.gov) weather data.
+
+## Installation
+Ensure Go is [installed](http://golang.org/doc/install.html) and your `PATH` includes the `$GOPATH/bin` directory
+
+```
+export PATH=$PATH:$GOPATH/bin
+```
+
+then install `wx` with
+
+```
+$ go get github.com/roualdes/wx
+```
+
+## Getting Started
+Only two commands are available `forecast` and `current`.  Each command takes an optional argument `ZIPCODE`, a five digit ZIP code corresponding to the location of interest.  Try
+
+```
+$ wx forecast 40502
+```
+
+If you don't specify a ZIP code, `wx` chooses uses 95926.  Print help with
+
+```
+$ wx help
+```
+
+
+
---
+++ wx.go
@@ -0,0 +1,223 @@
+// wx - Get NOAA weather.
+// Copyright (C) 2015 Edward A. Rouadles
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package main
+
+import (
+	"bytes"
+	"code.google.com/p/go-charset/charset"
+	_ "code.google.com/p/go-charset/data"
+	"encoding/xml"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// Body requests url and returns body.
+func Body(url string) (body []byte) {
+
+	// request url
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	defer resp.Body.Close()
+	
+	// read Body
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	
+	return
+}
+
+// LatLon retrieves LatLon from SOAP zipcode request.
+func LatLon(zip string) (latlon []string) {
+
+	// checks
+	if len(zip) != 5 {
+		fmt.Printf("Zipcode is not five numbers: %s\n", zip)
+		os.Exit(2)
+	}
+
+	// some data
+	zipreq := "http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?listZipCodeList=%s"
+	type LL struct {
+		LatLon  string   `xml:"latLonList"`
+	}
+
+	// request LatLon in XML
+	url := fmt.Sprintf(zipreq, zip)
+	body := Body(url)
+	
+	// parse LatLon from XML
+	var ll LL
+	if err := xml.Unmarshal(body, &ll); err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	latlon = strings.Split(ll.LatLon, ",")
+
+	return
+}
+
+type Day struct {
+	Value string `xml:"period-name,attr"`
+}
+type Cond struct {
+	Summary string `xml:"weather-summary,attr"`
+	Visibility string `xml:"value>visibility"` // miles
+}
+type Data struct {
+	Humidity string `xml:"humidity>value"` // relative
+	Condition []Cond `xml:"weather>weather-conditions"`
+	WindDirection string `xml:"direction>value"` // degrees true
+	WindSpeed []string `xml:"wind-speed>value"`  // gust, sustained, knots
+	Pressure string `xml:"pressure>value"` // Barometric, inches of mercury
+	Temperature []string `xml:"temperature>value"` // Farenheit
+	Description []string `xml:"wordedForecast>text"`
+}
+type Wx struct {
+	Place string `xml:"data>location>description"`
+	Time string `xml:"head>product>creation-date"`
+	HalfDay []Day `xml:"data>time-layout>start-valid-time"`
+	Parameter Data `xml:"data>parameters"`
+
+}
+
+// Weather retrieves NOAA weather updates for a LatLon coordinates.
+func Weather(ll []string) (wx Wx) {
+
+	// some data
+	freq := "http://forecast.weather.gov/MapClick.php?lat=%s&lon=%s&unit=0&lg=english&FcstType=dwml"
+
+	// request forecast in XML
+	url := fmt.Sprintf(freq, ll[0], ll[1])
+	body := Body(url)
+	reader := bytes.NewReader(body)
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReader
+
+	// decode XML
+	if err := decoder.Decode(&wx); err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	return
+}
+
+// Forecast prints a NOAA weather forecast
+func Forecast(wx Wx) {
+	for i, T := range wx.HalfDay[1:15] {
+		fmt.Printf("%s\n", T.Value)
+		lines := strings.SplitAfter(wx.Parameter.Description[i], ".")
+		for _, line := range lines {
+			fmt.Printf("  %s\n", strings.TrimLeft(line, " "))
+		}
+	}
+}
+
+// Current prints NOAA current weather
+func Current(wx Wx) {
+	fmt.Printf("  Summary: %s\n", wx.Parameter.Condition[14].Summary)
+	fmt.Printf("  Temperature (F): %s\n", wx.Parameter.Temperature[14])
+	fmt.Printf("  Dew Point (F): %s\n", wx.Parameter.Temperature[15])
+	fmt.Printf("  Humidity: %s\n", wx.Parameter.Humidity)
+	fmt.Printf("  Visibility: %s\n", wx.Parameter.Condition[15].Visibility)
+	fmt.Printf("  Wind Gust (max, knots): %s\n", wx.Parameter.WindSpeed[0])
+	fmt.Printf("  Wind Sustained (knots): %s\n", wx.Parameter.WindSpeed[1])
+	fmt.Printf("  Pressure (in): %s\n", wx.Parameter.Pressure)
+}
+
+func main() {
+	// custom help templates
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.Name}} {{if .Flags}}[global options] {{end}}command ZIPCODE
+
+VERSION:
+   {{.Version}}
+
+AUTHOR(S): 
+   {{range .Authors}}{{ . }}
+   {{end}}
+COMMANDS:
+   {{range .Commands}}{{join .Names ", "}}{{ "\t" }}{{.Usage}}
+   {{end}}{{if .Flags}}
+GLOBAL OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}{{end}}
+`
+	cli.CommandHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   wx {{.Name}}{{if .Flags}} [command options]{{end}} ZIPCODE
+`
+	
+	app := cli.NewApp()
+	app.Name = "wx"
+	app.Version = "0.1.0"
+	app.Usage = "Get NOAA weather."
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name: "Edward A. Roualdes",
+			Email: "edward.roualdes@uky.edu",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name: "forecast",
+			Aliases: []string{"f"},
+			Usage: "7 day NOAA weather forecast",
+			Action: func(c *cli.Context) {
+				zipcode := "95926" // default location
+				if len(c.Args()) > 0 {
+					zipcode = c.Args()[0]
+				}
+				ll := LatLon(zipcode) // get latlon
+				wx := Weather(ll)    // parse XML into Wx
+				fmt.Printf("%s @ %s\n", wx.Place, wx.Time)
+				Forecast(wx)
+			},
+		},
+		{
+			Name: "current",
+			Aliases: []string{"c"},
+			Usage: "current NOAA weather",
+			Action: func(c *cli.Context) {
+				zipcode := "95926" // default location
+				if len(c.Args()) > 0 {
+					zipcode = c.Args()[0]
+				}
+				ll := LatLon(zipcode) // get latlon
+				wx := Weather(ll)    // parse XML into Wx
+				fmt.Printf("%s @ %s\n", wx.Place, wx.Time)
+				Current(wx)
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
